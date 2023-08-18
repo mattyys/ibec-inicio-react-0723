0a086d3d4a7b6450b1834f7510de8d2226af35a4
Matias Alves <matyalves@gmail.com>
2023-08-18 20:11:37 -0300

    se actualiza lo hecho en clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 //se importa componente button
-import Button from './components/Button/Button';
+//import Button from './components/Button/Button';
+import ItemCount from './components/ItemCount/ItemCount';
 
 
 function App() {
@@ -10,10 +11,8 @@ function App() {
     //no puede devolver mas de 1 elemento, todo tiene que estar dentro de una etiqueta contenedora
     //se utiliza <Fragment> para o <> para envolver el contenido y al momento de compilar no se muestra, se diseñaron para no llenar de div las paginas
     <>
-
-     <Button title='Action 1' />
-     <Button title='pedito' />
-     <Button title='Action 3' />
+     
+     <ItemCount initial={1} stock={10} />
      </>
   )
 }
